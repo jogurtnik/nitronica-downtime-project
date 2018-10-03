6134cee39ae0e2801fc5be55b2e3affd633555c4
Remigiusz <remigiusz.widz@gmail.com>
2018-10-03 16:02:56 +0100

    Refactoring to multi-module Maven Project. Closes #1

---
+++ src/main/java/com/nitronica/nitronicadowntimeproject/NitronicaDowntimeProjectApplication.java
@@ -0,0 +1,12 @@
+package com.nitronica.nitronicadowntimeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitronicaDowntimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitronicaDowntimeProjectApplication.class, args);
+	}
+}
